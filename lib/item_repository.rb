287773c2db11d04c5b960709835c80358364c78b
Jonathan <jonacom@lissismore.com>
2016-03-09 16:42:58 -0700

    added input sanitation method to item repository

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -22,18 +22,22 @@ class ItemRepository
     items
   end
 
+  def input_sanitation(input, class_name)
+    input.is_a?(class_name)
+  end
+
   def find_by_id(item_id)
     item_id = item_id.to_i if item_id.class == String
     items.find { |item| item.id == item_id }
   end
 
   def find_by_name(item_name)
-    return nil unless item_name.is_a?(String)
+    return nil unless input_sanitation(item_name, String)
     items.find { |item| item.name.downcase == item_name.downcase }
   end
 
   def find_all_with_description(substring)
-    return [] unless substring.is_a?(String)
+    return [] unless input_sanitation(substring, String)
     items.find_all do |item|
       item.description.downcase.include?(substring.downcase)
     end
@@ -44,7 +48,7 @@ class ItemRepository
   end
 
   def find_all_by_price_in_range(price_range)
-    return [] unless price_range.is_a?(Range)
+    return [] unless input_sanitation(price_range, Range)
     items.find_all { |item| price_range.include?(item.unit_price) }
   end
 
